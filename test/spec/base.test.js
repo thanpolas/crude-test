5f1a33951061474dd8fe8ddccd73a45ceba545f9
Thanasis Polychronakis <thanpolas@gmail.com>
2014-07-04 12:15:26 +0300

    Add crude-test modules

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -1,12 +1,36 @@
 /**
- * @fileOverview Base API Surface tests.
+ * @fileOverview Test the exposed Test helpers API.
  */
-var Promise = require('bluebird');
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('Base API Surface', function() {
-  it('should do what...', function(){
-     // test!
+var UserEnt = require('../setup/user.ent');
+
+var tester = require('../lib/tester.lib');
+
+var crude = require('../..');
+var userFix = require('../fixtures/user.fix');
+
+describe('Test the exposed API Test Helper', function () {
+  this.timeout(5000);
+  it('should expose the tester', function() {
+    expect(crude.Test).to.be.a('function');
+  });
+
+  it('should execute...', function (done) {
+    tester.initActual().then(function () {
+      var testCrud = new crude.Test({
+        endpoint: '/user',
+        fixture: userFix.one,
+        stringAttr: 'firstName',
+        idAttr: '_id',
+        uniqueAttr: 'email',
+        Entity: UserEnt,
+      });
+
+      testCrud.run().then(done, done);
+
+    });
+
   });
 });
